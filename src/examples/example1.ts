9f1f5c1aeb496d30c06b25e56146646564f1645c
Ricky <rickyzoe0722@gmail.com>
2023-04-11 15:06:48 +0800

    update readme.

--- src/examples/example1.ts
+++ src/examples/example1.ts
@@ -5,7 +5,7 @@ import * as bcrypt from "bcrypt";
 這個值決定了產生哈希值時要運行的迭代次數，也就是加密的強度。
 值越高，產生哈希值所需的時間就越長，加密強度也越高，但也會對伺服器的效能產生一定的影響。
 一般建議的 salt rounds 值是10到12之間。
-另外，也請確認第一個參數password是一個字串或Buffer，如果不是的話也會拋出相同的錯誤訊息。
+另外，也請確認第一個參數password是一個字串或Buffer，如果不是的話會拋出錯誤訊息。
 */
 
 var user_hash_password: string = "";     // 使用者密碼(加密後).
